88c6c9e552e5d859353e8d518f03fce07f716f62
victoriahuynh <huynhvic@uw.edu>
2020-12-08 14:02:48 -0800

    merging and linking pages

--- madlibs-site/pages/setup/joingame.js
+++ madlibs-site/pages/setup/joingame.js
@@ -1,7 +1,20 @@
+import Game from "../../components/Game";
+import Head from "next/head";
+import Link from "next/link";
 
 
 const JoinGame = () => {
-    return <div>join game</div>
+    return (
+        <div>
+            <Head>
+                <title>Mad Libs</title>
+            </Head>
+            <h1>Mad Libs</h1>
+            <Link href="/game/gamelobby">
+                <a>Join Game</a>
+            </Link>
+        </div>
+    )
 }
 
 export default JoinGame;
